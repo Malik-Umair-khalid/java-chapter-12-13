d502df7e0e775d5ae985f35e9802997d14dc9d80
Malik-Umair-khalid <81845666+Malik-Umair-khalid@users.noreply.github.com>
2021-04-19 06:45:45 -0700

    Completed

---
+++ app.js
@@ -0,0 +1,119 @@
+// ***Queation : 1 ***
+
+// Write a program that takes a character (number or string)
+// in a variable & checks whether the given input is a
+// number, uppercase letter or lower case letter. (Hint: ASCII
+// // codes:- A=65, Z=90, a=97, z=122).
+
+// *** Q : 2 ***
+
+// Write a JavaScript program that accept two integers and
+// display the larger. Also show if the two integers are equal.
+
+// var integer1 = +prompt("Enter First Integer")
+// var integer2 = +prompt("Enter second Integer")
+
+// if (integer1 > integer2){
+//     alert("First Integer is larger")
+// }
+// else if (integer1 < integer2){
+//     alert("Second Integer is larger")
+// }
+// else if (integer1 === integer2){
+//     alert("Both are Equal")
+// }
+
+//  *** Q : 3 ***
+
+// Write a program that takes input a number from user &
+// state whether the number is positive, negative or zero.
+
+// var number = +prompt("Enter Your Number")
+
+// if (number > 0){
+//     alert("The Number is Positive")
+// }
+// else if (number < 0){
+//     alert("The Number is Negitive")
+// }
+// else if (number === 0){
+//     alert("The number is Zero")
+// }
+
+//  *** Q : 4 *** 
+// Write a program that takes a character (i.e. string of
+//     length 1) and returns true if it is a vowel, false otherwise
+
+// var word = prompt("Write")
+// if (word === "a" || word === "e" || word === "i" || word === "o" || word === "u"){
+//     alert("The Letter is Vowel")
+// }
+// else {
+//     alert("The Letter Is Not Vowel")
+// }
+
+//  *** Q : 5 *** 
+
+// Write a program that
+// a. Store correct password in a JS variable.
+// b. Asks user to enter his/her password
+// c. Validate the two passwords:
+// i. Check if user has entered password. If not, then
+// give message “ Please enter your password”
+// ii. Check if both passwords are same. If they are
+// same, show message “Correct! The password you
+// entered matches the original password”. Show
+// “Incorrect password” otherwise.
+
+// var password = "pak123";
+// var askPassward = prompt("Enter The Passward.Hint(pak123)")
+
+// if (askPassward === password){
+//     alert ("Correct Passward")
+// }
+// else if (askPassward != password){
+//     alert("Incorrect Passward")
+// }
+// else if(askPassward === ""){
+//     alert("Please Enter Your Passward")
+// }
+
+//  *** Q : 6 ***
+
+// This if/else statement does not work. Try to fix it:
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";
+// else
+// greeting = "Good evening";
+// }
+
+// var hour = 13;
+// if (hour > 18) {
+//  alert ("Good day")
+// }
+// else{
+//  alert("Good evening")
+// }
+
+//  *** Q : 7 ***
+
+// Write a program that takes time as input from user in 24
+// hours clock format like: 1900 = 7pm. Implement the
+// following case using if, else & else if statements
+
+// var time = +prompt("What is Time")
+// if (time >= 0000 && time < 1200){
+//     alert("Good Morning")
+// }
+// else if (time >= 1200 && time < 1700){
+//     alert("Good after noon")
+// }
+// else if (time >= 1700 && time < 2100){
+//     alert("Good Evening")
+// }
+// else if (time >= 2100 && time < 2359){
+//     alert("Good Night")
+// }
+
